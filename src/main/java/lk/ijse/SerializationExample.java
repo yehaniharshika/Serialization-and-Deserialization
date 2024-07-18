3c1246a56daa1a0757a6db29801a8030cc7ec3d5
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-07-18 11:56:20 +0530

    Serialization complete

--- src/main/java/lk/ijse/SerializationExample.java
+++ src/main/java/lk/ijse/SerializationExample.java
@@ -1,7 +1,30 @@
 package lk.ijse;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class SerializationExample {
     public static void main(String[] args) {
-        
+        Student student = new Student("Yehani",12,"A");
+        String filename = "student.ser";
+
+        //serialization
+        try {
+            //Saving of object in a file
+            FileOutputStream fileOutputStream = new FileOutputStream(filename);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            // Method for serialization of object
+            objectOutputStream.writeObject(student);
+            System.out.println("Serialized data is saved in student.ser");
+            System.out.println(student);
+            objectOutputStream.close();
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
