4891411e2550c3abc7fb9c8332b3ea515bd555c3
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2024-07-18 11:03:47 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/SerializationExample.java
@@ -0,0 +1,7 @@
+package lk.ijse;
+
+public class SerializationExample {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/lk/ijse/Student.java
@@ -0,0 +1,24 @@
+package lk.ijse;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+    private String grade;
+
+    public Student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+}
